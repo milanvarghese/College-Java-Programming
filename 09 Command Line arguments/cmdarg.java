83465e36a71f1d29bcabe73a52327fce9487b683
Milan <35422302+milanvarghese@users.noreply.github.com>
2021-02-19 10:45:10 +0530

    Add files via upload

---
+++ 09 Command Line arguments/cmdarg.java
@@ -0,0 +1,14 @@
+import java.lang.String;
+
+public class cmdarg
+{
+	public static void main(String args[])
+	{	
+		int i;
+		i=args.length;
+		System.out.println("No of arguments: "+i);
+	}
+}
+
+
+//run this file as java cmdarg.java 23 435 56
